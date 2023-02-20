25993d6bede54d926efa6d00cb98b26f7ff69bc8
softlang-net <x915927@icloud.com>
2023-02-20 12:26:36 +0800

    Update deploy.js

--- cmd/npm/deploy.js
+++ cmd/npm/deploy.js
@@ -1,26 +1,34 @@
-const { spawnSync, spawn } = require('node:child_process');
-const ls = spawn('bash', ['-c', 'docker exec -i node-complier pwd']);
-console.log('hellow');
-ls.stdout.on('data', (data) => {
-  console.log(`____stdout: ${data}`);
-});
+const { spawnSync } = require('node:child_process');
 
-ls.stderr.on('data', (data) => {
-  console.error(`____stderr: ${data}`);
-});
+var task_step = 0;
 
-ls.on('close', (code) => {
-  console.log(`____child process exited with code ${code}`);
-  if (code > 0) {
-    console.log('____spawn has error');
-  }
-});
-
-const dc = spawnSync('docker', ['exec', '-i', 'node-complier', 'ls', '-la'],
-    { stdio: 'inherit', env:{DOCKER_CONTEXT: 'default'}});
-
-if(dc.status > 0){
-  console.error(`>>error:${dc.status}`);
-}else{
-  console.log('>> spawn sync success');
+function getNow() {
+  const dateObj = new Date();
+  let year = dateObj.getFullYear();
+  let month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
+  let date = ('0' + dateObj.getDate()).slice(-2);
+  let hour = ('0' + dateObj.getHours()).slice(-2);
+  let minute = ('0' + dateObj.getMinutes()).slice(-2);
+  let second = ('0' + dateObj.getSeconds()).slice(-2);
+  const time = `${year}/${month}/${date} ${hour}:${minute}:${second}`;
+  return time
 }
+
+/**
+ * @param {string} task The task info
+ * @param {string} context The docker context
+ * @param {ReadonlyArray<string>} cmd_args The command args
+ */
+function exec(task, context, ...cmd_args) {
+  task_step++;
+  console.log(`🟢 ${task_step}. ${getNow()} -- ${task}`)
+  const cmd = spawnSync('docker', cmd_args, { stdio: 'inherit', env: { DOCKER_CONTEXT: context } });
+  if (cmd.status > 0) {
+    console.error(`🔴 context=${context}, command=${command}, :${dc.status}`);
+  } else {
+    console.log('>> spawn sync success');
+  }
+}
+
+exec("text01", 'default', ['service', 'ls'])
+exec("text01", 'default', ['ps', '-a'])
