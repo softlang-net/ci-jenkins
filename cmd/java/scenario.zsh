f7b2364192f371d640aa04abdec14d0c55f567bf
softlang-net <x915927@icloud.com>
2023-02-25 21:28:52 +0800

    java node python
    prepare & tidy

---
+++ cmd/java/scenario.zsh
@@ -0,0 +1,154 @@
+#!/bin/bash 
+set -e;
+#cd $(dirname $0);
+
+# ❓❓❓ export vars by jenkins
+ci_compose_cpus=${ci_compose_cpus:-1}
+ci_compose_memory=${ci_compose_memory:-1024M}
+ci_compose_replicas=${ci_compose_replicas:-2}
+ci_compose_service=${ci_compose_service}
+ci_router_prefix=${ci_router_prefix:-$ci_compose_service}
+ci_dockerfile=${ci_compose_dockerfile:-Dockerfile}
+ci_git_project=${ci_git_project}
+ci_git_base=${ci_git_base}
+ci_env_profile=${ci_env_profile:-env} #⛔docker env profile, 如：application.env.yaml
+# ❓❓❓
+# 🔻🔻🔻🔻🔻🔻🔻🔻🔻
+CI_ENV="prd"
+CI_DOCKER_CONTEXT_DEPLOY="${CI_ENV}-zyb" #⛔部署service的docker环境
+CI_DOCKER_CONTEXT_BUILD="default" #⛔构建image的docker环境
+CI_DOCKER_NETWORK="zyb_prd_over" #⛔service运行的docker网络
+ci_service_port=${ci_service_port:-81} #⛔service端口（traefik-loadbalancer.server.port）
+ci_compose_image="ci2-internal.x-b.ltd/zyb/${CI_ENV}/${ci_compose_service}:${CI_ENV}-${BUILD_NUMBER}" #⛔镜像tag&仓库
+# /api-xxx-xxx
+ci_env_app_path=${ci_router_prefix} #⛔docker env app_path
+# work_dir
+ci_work_dir="/opt/deploy/${CI_ENV}/${JOB_NAME}" #⛔构建目录
+ci_git_host="ssh://git@ci2.dev:10022" #git clone
+ci_git_devops=${ci_git_host}/zyb/devops.git
+ci_git_env_config=env/zyb_${CI_ENV}.conf #⛔env-config@git
+# 🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺
+
+echo ">>📌 1. environments"
+env
+
+echo ">>📌 2. clone from git"
+# 1. git clone && git archive --remote=ssh://git@xxx <branch | HEAD> <prefix_path/xxx/xx> 
+rm ${ci_work_dir} -rf && mkdir -p ${ci_work_dir} && cd ${ci_work_dir}
+git archive --remote=${ci_git_devops} master ${ci_git_env_config} | tar xO > ./config.env
+if [[ ! -s ./config.env ]]; then echo "❌ERR: empty ${ci_git_env_config}" && exit 1;fi
+
+git clone -b $ci_git_branch ${ci_git_host}/${ci_git_project} ${ci_work_dir}/src
+cd $(realpath src/${ci_git_base}) # change the real source-code work-dir
+
+# 2. build
+# 执行打包
+echo ">>📌 3. mvn clean package -q -Dmaven.test.skip=true -f pom.xml"
+#mvn clean package -q -Dmaven.test.skip=true -f pom.xml
+# ls ./target
+mvn_bash_c="mvn clean package -s ~/m2_settings.xml -q -Dmaven.test.skip=true -f pom.xml"
+docker exec -i -w $PWD maven3-java8.dev bash -c "${mvn_bash_c}"
+
+LAST_ERROR_CODE=$?
+if [ ! "$LAST_ERROR_CODE" = "0" ]
+then
+  echo ">>❌ mvn clean package fail(@docker): $LAST_ERROR_CODE"
+  exit $LAST_ERROR_CODE
+fi
+
+# 3. docker build
+# git download single file
+# git archive --remote=git@github.com:foo/bar.git --prefix=path/to/ HEAD:path/to/ |  tar xvf -
+echo ">>📌 4. build image && push to registry"
+export DOCKER_CONTEXT=${CI_DOCKER_CONTEXT_BUILD}
+docker image prune -f
+docker build --network host --force-rm --compress \
+    --build-arg profile=${ci_env_profile} \
+    --build-arg app_path=${ci_env_app_path} \
+    -t "${ci_compose_image}" \
+    -f ${ci_dockerfile} \
+    . # Dockerfile context path
+
+docker push ${ci_compose_image}
+docker rmi ${ci_compose_image} || echo ">>❌❌ docker rmi faild: ${ci_compose_image}"
+
+# deploy service
+# --env-file .env \
+echo ">>📌 5. deploy to farm"
+# -- env-file
+env_list=""
+function read_env_file() {
+    prefix=${1:-" -e "}
+    while read LINE || [[ -n ${LINE} ]]
+    do 
+        s1=${LINE# }
+        s1=${s1% }
+        if [ ${#s1} = 0 ] || [ ${s1:0:1} = "#" ]
+        then
+            continue
+        fi
+        env_list+=${prefix}${s1}
+    done < ${ci_work_dir}/config.env
+    echo "🔑🔑" && echo "[${env_list}]" && echo "🔑🔑"
+}
+
+# deploy to docker_context@remote
+JAVA_OPS=`echo "$ci_compose_memory" | tr -cd "[0-9]"`
+JAVA_OPS=-Xmx$[$JAVA_OPS/2]m
+
+NEED_UPDATE_SERVICE=1
+export DOCKER_CONTEXT=${CI_DOCKER_CONTEXT_DEPLOY}
+#docker service inspect --pretty ${ci_compose_service} || NEED_UPDATE_SERVICE=0
+docker service inspect --format service_id={{.ID}} ${ci_compose_service} || NEED_UPDATE_SERVICE=0
+
+if [ "${NEED_UPDATE_SERVICE}" = "1" ] 
+then
+    echo ">>✅ Start update service..."
+    env_list="" && read_env_file " --env-add " && \
+    docker service update -d --env-add "JAVA_OPS=${JAVA_OPS}" \
+        --env-add "profile=${ci_env_profile}" --env-add "app_path=${ci_env_app_path}" ${env_list} \
+        --label-add "io.portainer.accesscontrol.public" \
+        --label-add "cigo=zyb" \
+        --label-add "traefik.http.routers.${ci_compose_service}.entrypoints=zyb" \
+        --label-add "traefik.http.routers.${ci_compose_service}.service=${ci_compose_service}" \
+        --label-add "traefik.http.routers.${ci_compose_service}.rule=PathPrefix(\`${ci_router_prefix}\`)" \
+        --label-add "traefik.http.services.${ci_compose_service}.loadbalancer.server.port=${ci_service_port}" \
+        --limit-cpu ${ci_compose_cpus} \
+        --limit-memory ${ci_compose_memory} \
+        --replicas ${ci_compose_replicas} \
+        --update-delay 20s \
+        --restart-delay 60s \
+        --restart-window 120s \
+        --restart-max-attempts 3 \
+        --image ${ci_compose_image} \
+        ${ci_compose_service}
+    echo ">>✅ update service success."
+else
+    echo ">>🟢 Start create service..."
+    env_list="" && read_env_file " -e " && \
+    docker service create -d --mode replicated -e "JAVA_OPS=${JAVA_OPS}" \
+        -e "profile=${ci_env_profile}" -e "app_path=${ci_env_app_path}" ${env_list} \
+        --network ${CI_DOCKER_NETWORK} \
+        --update-parallelism 1 \
+        --update-delay 20s \
+        --restart-delay 60s \
+        --restart-window 120s \
+        --restart-max-attempts 3 \
+        --name ${ci_compose_service} \
+        --limit-cpu ${ci_compose_cpus} \
+        --limit-memory ${ci_compose_memory} \
+        --replicas ${ci_compose_replicas} \
+        --label "io.portainer.accesscontrol.public" \
+        --label "cigo=zyb" \
+        --label "traefik.enable=true" \
+        --label "traefik.http.routers.${ci_compose_service}.entrypoints=zyb" \
+        --label "traefik.http.routers.${ci_compose_service}.service=${ci_compose_service}" \
+        --label "traefik.http.routers.${ci_compose_service}.rule=PathPrefix(\`${ci_router_prefix}\`)" \
+        --label "traefik.http.services.${ci_compose_service}.loadbalancer.server.port=${ci_service_port}" \
+        ${ci_compose_image} # create new service
+
+    echo ">>🟢 create service success"
+fi
+
+echo ">>📌 6. service inspect"
+docker service inspect --pretty ${ci_compose_service}
---
+++ cmd/node/scenario.zsh
@@ -0,0 +1,147 @@
+#!/bin/bash
+#------deploy for h5 with NPM ------
+TZ=UTC-8
+set -e;
+if [ ! "$1" = "" ]
+then
+    . $1
+fi
+# 🔻🔻🔻🔻🔻🔻🔻🔻🔻
+ci_env_profile=${ci_env_profile:-dev}
+ci_env_image_tag=${ci_env_image_tag:-${BUILD_NUMBER:-latest}}
+ci_env_registry=${ci_env_registry:-"image.ci:5000"}
+
+ci_docker_context_deploy=${ci_docker_context_deploy:-default}
+ci_docker_context_build=${ci_docker_context_build:-default}
+ci_container_git=${ci_container_git:-"git-docker-cli"}
+
+# runtime resource config
+ci_compose_cpus=${ci_compose_cpus:-1}
+ci_compose_memory=${ci_compose_memory:-512M}
+ci_compose_replicas=${ci_compose_replicas:-1}
+ci_compose_service_name=${ci_compose_service_name:-$JOB_BASE_NAME}
+ci_compose_service_port=${ci_compose_service_port:-80}
+ci_compose_network=${ci_compose_network}
+
+ci_router_entry=${ci_router_entry:-traefik}
+ci_router_prefix=${ci_router_prefix:-"/$ci_compose_service"}
+
+# compiler config
+ci_dockerfile=${ci_dockerfile:-"/opt/make/compilers/npm/Dockerfile"}
+ci_git_project=${ci_git_project}
+ci_git_branch=${ci_git_branch:-$ci_env_profile}
+ci_git_src_dir=${ci_git_src_dir:-""}
+ci_workspace=${ci_workspace:-"/opt/make/workspace"}
+
+# env_profile
+ci_compose_image="${ci_env_registry}/${ci_env_profile}/${ci_compose_service_name}:${ci_env_image_tag}" #⛔镜像tag&仓库
+# work_dir for source code & compiler
+ci_work_dir="$ci_workspace/$ci_env_profile/${JOB_NAME:-$ci_compose_service_name}" #⛔构建目录
+# 🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺
+
+# ----- for detect errors -----
+LAST_ERROR_CODE=0
+function check_docker_return() {
+    if [ ! "$LAST_ERROR_CODE" = "0" ]
+    then
+        echo "❌>> $1, ERROR_CODE=$LAST_ERROR_CODE"
+        exit $LAST_ERROR_CODE
+    fi
+}
+# ----- for detect errors -----
+
+echo ">>📌 1. environments=ci_env_profile"
+#env # print env variables
+
+# switch to the build docker_context
+export DOCKER_CONTEXT=${ci_docker_context_build}
+
+echo ">>📌 2. clone from git"
+git_bash_c1="rm -rf ${ci_work_dir} && mkdir -p ${ci_work_dir}"
+git_bash_c2="git clone -b $ci_git_branch $ci_git_project ${ci_work_dir}/src"
+
+docker exec -i -u git $ci_container_git bash -c "${git_bash_c1}"
+LAST_ERROR_CODE=$?
+check_docker_return "Prepare work_dir failed"
+
+docker exec -i -u git $ci_container_git bash -c "${git_bash_c2}"
+LAST_ERROR_CODE=$?
+check_docker_return "Git clone branch failed"
+
+# 3. docker build
+export DOCKER_CONTEXT=${ci_docker_context_build}
+# git archive --remote=git@github.com:foo/bar.git --prefix=path/to/ HEAD:path/to/ |  tar xvf -
+echo ">>📌 4. build image && push to registry"
+#docker image prune -f
+cat <<EOF | docker exec -i -w $ci_work_dir/src/$ci_git_src_dir $ci_container_git bash
+docker build --force-rm --compress \\
+--build-arg ci_env_profile=${ci_env_profile} \\
+--build-arg ci_router_prefix=${ci_router_prefix} \\
+-t ${ci_compose_image} -f ${ci_dockerfile} .
+EOF
+
+echo ">> finish early"
+exit 0;
+
+docker push ${ci_compose_image}
+docker rmi ${ci_compose_image} || echo ">>❌❌ docker rmi faild: ${ci_compose_image}"
+
+# deploy service
+# --env-file .env \
+echo ">>📌 6. deploy to farm"
+# deploy to docker_context@remote
+NEED_UPDATE_SERVICE=1
+export DOCKER_CONTEXT=${CI_DOCKER_CONTEXT_DEPLOY}
+#docker service inspect --pretty ${ci_compose_service} || NEED_UPDATE_SERVICE=0
+docker service inspect --format service_id={{.ID}} ${ci_compose_service} || NEED_UPDATE_SERVICE=0
+
+if [ "${NEED_UPDATE_SERVICE}" = "1" ] 
+then
+    echo ">>✅ Update service..."
+    docker service update -d \
+        --label-add "io.portainer.accesscontrol.public" \
+        --label-add "cigo=zyb" \
+        --label-add "traefik.http.routers.${ci_compose_service}.entrypoints=zyb" \
+        --label-add "traefik.http.routers.${ci_compose_service}.service=${ci_compose_service}" \
+        --label-add "traefik.http.routers.${ci_compose_service}.rule=PathPrefix(\`${ci_router_prefix}\`)" \
+        --label-add "traefik.http.services.${ci_compose_service}.loadbalancer.server.port=${ci_service_port}" \
+        --limit-cpu ${ci_compose_cpus} \
+        --limit-memory ${ci_compose_memory} \
+        --replicas ${ci_compose_replicas} \
+        --image ${ci_compose_image} \
+        ${ci_compose_service}
+    echo ">>✅ Update service success."
+else
+    echo ">>🟢 Create service..."
+    docker service create -d --mode replicated \
+        --network ${CI_DOCKER_NETWORK} \
+        --update-parallelism 1 \
+        --update-delay 20s \
+        --restart-delay 30s \
+        --restart-window 0 \
+        --restart-max-attempts 10 \
+        --name ${ci_compose_service} \
+        --limit-cpu ${ci_compose_cpus} \
+        --limit-memory ${ci_compose_memory} \
+        --replicas ${ci_compose_replicas} \
+        --label "io.portainer.accesscontrol.public" \
+        --label "cigo=zyb" \
+        --label "traefik.enable=true" \
+        --label "traefik.http.routers.${ci_compose_service}.entrypoints=zyb" \
+        --label "traefik.http.routers.${ci_compose_service}.service=${ci_compose_service}" \
+        --label "traefik.http.routers.${ci_compose_service}.rule=PathPrefix(\`${ci_router_prefix}\`)" \
+        --label "traefik.http.services.${ci_compose_service}.loadbalancer.server.port=${ci_service_port}" \
+        ${ci_compose_image} # create new service
+
+    echo ">>🟢 Create service success"
+fi
+
+echo ">>📌 7. service inspect"
+docker service inspect --pretty ${ci_compose_service}
+
+cat <<EOF | docker exec -i -w $ci_work_dir/src/$ci_git_src_dir $ci_container_git bash
+docker build --force-rm --compress \\
+--build-arg ci_env_profile=${ci_env_profile} \\
+--build-arg ci_router_prefix=${ci_router_prefix} \\
+-t ${ci_compose_image} -f ${ci_dockerfile} .
+EOF
